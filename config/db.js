371c94668286d5c2837e92950197403019cbec53
kshitij3188 <kshitijk3188@gmail.com>
2021-03-16 09:36:56 +0530

    starting react

--- config/db.js
+++ config/db.js
@@ -3,18 +3,20 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
 
-        console.log('mongoDB Connected...');
-    } catch(err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
 
 module.exports = connectDB;
